f1d84024aac245708c79e8139b692557bb2afaa7
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-08-17 11:46:15 -0500

    Se elimina update.bat y se actualiza achivo de sincronizacion de git, tambien se actualiza requirements.windows.txt

--- github_sync.py
+++ github_sync.py
@@ -58,5 +58,8 @@
 import git 
 
 repo = git.Repo('C:/Sites/ocr-python')
+repo.git.reset('--hard')
+
 origin = repo.remote(name='origin')
 origin.pull('noindex-word')
+print("El repositorio ha sido actualizado")
